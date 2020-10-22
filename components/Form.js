4b95cbb88c830ab3fe409ed7fd9646217b480b95
Anita RASOA <anita.ras@onja.org>
2020-10-22 11:18:27 +0300

    Creating the modal and form inside

--- components/Form.js
+++ components/Form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Form(props) {
-    const { show, handleSelect, openModal} = props;
+    const { show, handleSelect, openModal, handleGuest} = props;
     return (
         <form className="add_container">
             <select className="city" name="Location" onChange={handleSelect}>
@@ -11,8 +11,10 @@ function Form(props) {
                 <option value="Oulu">Oulu</option>
                 <option value="Vaasa">Vaasa</option>
             </select>
-            <input className="add_guest" type="number" name="guestNum" placeholder="Add guests" onChange={props.handleGuest} />
-            {!show && <button type="button" onClick={openModal} className="add_button">Search</button>}
+            <input className="add_guest" type="number" name="guestNum" placeholder="Add guests" onChange={handleGuest} />
+            <p className="container_btn">
+                {!show && <button type="button" onClick={openModal} className="add_button">Search</button>}
+            </p>
         </form>
     )
 }
--- components/Modal.js
+++ components/Modal.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Form from '../components/Form';
 
 function Modal(props) {
   const { show, closeModal } = props;
 
   const modal = 
   (<>
-    <div className={show ? 'overlay' : 'hide'} onClick={closeModal}>
+    <div className={show ? 'overlay' : 'hide'}>
       <div className={show ? "modal" : "hide"}>
         <button onClick={closeModal}>X</button>
-        <h1>Modal heading</h1>
-        <p>This is modal content</p>
+        <Form />
       </div>
     </div> 
   </>)
